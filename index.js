d7af8c93ef522716a9c4024797e7685780b5a9d5
andi25 <adallapozza@hotmail.com>
2021-08-12 13:08:25 -0300

    Capturo nombre y edad

---
+++ index.js
@@ -0,0 +1,11 @@
+function capturar(){
+    // console.log("capturar");
+    function Persona(nombre,edar){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    // let nombreCapturar = document.getElementById('nombre').value;
+    let nombreCapturar = $('#nombre').val();
+    let edadCapturar = $('#edad').val();
+    console.log(nombreCapturar, edadCapturar);
+};
