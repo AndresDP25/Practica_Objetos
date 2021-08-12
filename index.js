65e0e27022828d6f4b25d008a757cc5fbf891845
andi25 <adallapozza@hotmail.com>
2021-08-12 14:37:00 -0300

    temrmine tutorial

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function capturar(){
     // console.log("capturar");
-    function Persona(nombre,edar){
+    function Persona(nombre,edad){
         this.nombre = nombre;
         this.edad = edad;
-    }
+    };
     // let nombreCapturar = document.getElementById('nombre').value;
-    let nombreCapturar = $('#nombre').val();
-    let edadCapturar = $('#edad').val();
+    var nombreCapturar = $('#nombre').val();
+    var edadCapturar = $('#edad').val();
+
     console.log(nombreCapturar, edadCapturar);
+
+    nuevoSujeto = new Persona(nombreCapturar,edadCapturar);
+    console.log(nuevoSujeto);
+    agregar();
+};
+
+var baseDatos = [];
+function agregar(){
+    baseDatos.push(nuevoSujeto);
+    
+    $('#table').append('<tbody><td>'+nuevoSujeto.nombre+'</td><td>'+nuevoSujeto.edad+'</td></tbody>');
+    // document.getElementById('table').innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>'+nuevoSujeto.edad+'</td></tbody>';
 };
